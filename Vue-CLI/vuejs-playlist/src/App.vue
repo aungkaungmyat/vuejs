d8c2395c2c7b9813fb025631ff078fa7bbc2d3cf
aungkaungmyat <amyat@purdue.edu>
2017-06-07 00:08:46 +0630

    got some primitive and reference

--- Vue-CLI/vuejs-playlist/src/App.vue
+++ Vue-CLI/vuejs-playlist/src/App.vue
@@ -1,39 +1,39 @@
 <template>
-  <!-- <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div> -->
   <div>
-    <h1>{{title}}</h1>
-    <ninjas></ninjas>
+    <app-header v-bind:title="title"></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <app-footer v-bind:title="title"></app-footer>
   </div>
+
 </template>
 
 <script>
-// import Hello from './components/Hello'
-import Ninjas from './Ninjas.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 
 export default {
-  // name: 'app',
-  // components: {
-  //   Hello
-  // }
   components: {
-    'ninjas': Ninjas
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
   },
   data(){
-    return{
-      title: 'Ninja App'
+    return {
+      ninjas: [
+                {name: 'Ryu', speciality: 'Vue Components', show: false},
+                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+                {name: 'Hitoshi', speciality: 'Click Events', show: false},
+                {name: 'Tango', speciality: 'Conditionals', show: false},
+                {name: 'Kami', speciality: 'Webpack', show: false},
+                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+            ],
+      title: "Vue Ninjas"
     }
-  },
-  methods:{
-
   }
 }
 </script>
 
 <style scoped>
-h1{
-    color: purple;
-}
+
 </style>
---
+++ Vue-CLI/vuejs-playlist/src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <footer>
+      <p>{{copyright}} {{title}}</p>
+    </footer>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  props:{
+    title:{
+      type: String
+    }
+  },
+  data(){
+    return {
+        copyright: 'Copyright 2017'
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer{
+  background: #222;
+  padding: 6px;
+}
+
+p{
+  color: lightgreen;
+  text-align: center;
+}
+</style>
