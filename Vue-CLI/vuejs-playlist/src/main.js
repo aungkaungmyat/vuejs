fd33af436deb2f853f38890e0b7e42155b9e3ac2
aungkaungmyat <amyat@purdue.edu>
2017-06-07 23:35:58 +0630

    custom directives

--- Vue-CLI/vuejs-playlist/src/main.js
+++ Vue-CLI/vuejs-playlist/src/main.js
@@ -3,9 +3,31 @@ import App from './App'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
+
+//Custom directives
+Vue.directive('rainbow',{
+  bind(el,binding,vnode){
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+
+Vue.directive('theme',{
+  bind(el,binding,vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = "1200px";
+    }
+    else if(binding.value == 'narrow'){
+      el.style.maxWidth = "560px";
+    }
+    if(binding.arg == 'column'){
+      el.style.background = "#ddd";
+      el.style.padding = '20px';
+    }
+  }
+})
 // import Ninjas from './Ninjas.vue'
 //
-export const bus  =  new Vue();
+// export const bus  =  new Vue();
 // Vue.component('ninjas', Ninjas);
 Vue.config.productionTip = false
 
